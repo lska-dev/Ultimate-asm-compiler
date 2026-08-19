db101d0ba07c4f652752a3ae80ad00b5bbe7abc7
lska-dev <lukicevila5@gmail.com>
2026-08-19 15:00:01 +0300

    add expressions and float

--- processors/exemple.py
+++ processors/exemple.py
@@ -1,5 +1,8 @@
 class parram:
     def __init__(self):
+
+        self.name = "exemple"
+
         self.mem_len =      0xffffff # Объем адресного пространства (слов)
         self.max_number = 0xffffffff # Максимальное обрабатываемое число
         self.width =              32 # Разрядность слова
@@ -17,24 +20,13 @@ class parram:
 # TODO: ISA
 # " Имя операции " : eval { кодирование }
             'jmp' : "0xFF + get_label(arg1) << 32 ", # ПРИМЕР
-
-""" =============================
-Встроенные функции:
-    get_label ( name )  
-    number_string ( str )
-    get_register ( name ) 
-++++++++++++++++++++++++++++++++
-Агрументы:
-    arg1
-    arg2
-    arg3    
-    arg4    
-============================= """
             # Встроенные
             'data=': 'spec',    # НЕ УДАЛЯТЬ
             '#define': 'spec',  # НЕ УДАЛЯТЬ
             'fn:': 'spec',      # НЕ УДАЛЯТЬ
             ':': 'spec',        # НЕ УДАЛЯТЬ
+
+            "word" : "number_string(arg1) + (number_string(arg2) << 32)",
         }
 
         self.LEN = {            # Обязательно
@@ -43,6 +35,8 @@ class parram:
             "#define" : 1,      # НЕ УДАЛЯТЬ
             "fn:" : 1,          # НЕ УДАЛЯТЬ
             ":" : 1,            # НЕ УДАЛЯТЬ
+
+            "word" : 1,
         }
 
         self.functions = {      # Обязательно
